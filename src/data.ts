627fc3e384ed4f305d3899a5f6e86202a005ed0d
Rene Charbonneau <me@charbonneaurene.com>
2024-12-31 13:28:31 -0500

    modified data.ts export

--- src/data.ts
+++ src/data.ts
@@ -1,11 +1,14 @@
 let SystemPurposes;
+let defaultSystemPurposeId;
 
 if (process.env.PROJECT_ENV === 'cx-agi') {
-  SystemPurposes = require('./data.cx-agi').SystemPurposes;
+    SystemPurposes = require('./data.cx-agi').SystemPurposes;
+    defaultSystemPurposeId = require('./data.cx-agi').defaultSystemPurposeId;
 } else if (process.env.PROJECT_ENV === 'home-agi') {
-  SystemPurposes = require('./data.home-agi').SystemPurposes;
+    SystemPurposes = require('./data.home-agi').SystemPurposes;
+    defaultSystemPurposeId = require('./data.home-agi').defaultSystemPurposeId;
 } else {
-  throw new Error('Invalid PROJECT_ENV value. Please set the correct environment variable.');
+    throw new Error('Invalid PROJECT_ENV value. Please set the correct environment variable.');
 }
 
-export { SystemPurposes };
+export { SystemPurposes, defaultSystemPurposeId };
